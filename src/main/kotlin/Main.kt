33aba8d32d2f346b3bfa40c0c37b06e1054077ad
Cathybiwott <biwottcathy@gmail.com>
2023-03-09 08:56:28 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,42 @@
+fun main() {
+    val text=("Hello world!")
+    println(text[0])
+    println(items)
+    var addition:(num1:20,num2:40)
+
+
+
+
+
+}
+
+fun friends(text:String,:String):String{
+
+
+}
+
+fun values(num1:Int,num2:Int,num3:Int,num4:Int):Int{
+    var ("Teacher")
+
+}
+
+fun stmt(txt:String){
+    val x="banana"
+    println(txt[1])
+    println(txt[2])
+    println(txt[3])
+    println(txt[4])
+    println(txt[5])
+}
+
+fun items(name:String,name2:String){
+    val x=("Hello"+"world")
+    val x=("Hello" +"  "+"world")
+    println()
+}
+
+class data(num1: Int,num2: Int,num3: Int,num4: Int){
+    var sum=num1+num2
+    var
+}
+
